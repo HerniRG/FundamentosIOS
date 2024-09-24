739930723dac65b0fcb36449863eeafc5a92f98a
hernirg <hernanrg85@gmail.com>
2024-09-24 16:04:15 +0200

    visualización en lista de heroes si es favorito

--- FundamentosIOS/Presentation/View/HeroesTableViewCell.swift
+++ FundamentosIOS/Presentation/View/HeroesTableViewCell.swift
@@ -15,18 +15,26 @@ class HeroesTableViewCell: UITableViewCell {
     @IBOutlet weak var heroTitleLabel: UILabel!
     @IBOutlet weak var heroDescriptionLabel: UILabel!
     @IBOutlet weak var viewCell: UIView!
+    @IBOutlet weak var favoriteImageView: UIImageView!
     
     // MARK: - Configuration
     func configure(with hero: Hero) {
-        // Configuramos las propiedades visuales de la celda
+        
         viewCell.backgroundColor = UIColor.systemIndigo
         viewCell.layer.borderWidth = 3
         viewCell.layer.cornerRadius = 10
+        
         heroImageView.layer.borderWidth = 2
         heroImageView.layer.cornerRadius = 10
         
-        // Asignamos las propiedades del modelo Hero a los componentes de la celda
+        favoriteImageView.translatesAutoresizingMaskIntoConstraints = false
+        favoriteImageView.widthAnchor.constraint(equalToConstant: 20).isActive = true
+        favoriteImageView.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        favoriteImageView.image = UIImage(systemName: hero.favorite ? "star.fill" : "star")
+        favoriteImageView.tintColor = .yellow
+        
         heroTitleLabel.text = hero.name
+        heroTitleLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         heroDescriptionLabel.text = hero.description
         heroImageView.setImage(from: hero.photo)
     }
